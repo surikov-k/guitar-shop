1247ba4d15ae3e83ff29508ff9d07536096db127
Konstantin Surikov <surikov@gmail.com>
2023-02-10 11:23:33 +0300

    ♻️ [refactor:] add an enum for the app routs

--- apps/webapp/src/app/app.tsx
+++ apps/webapp/src/app/app.tsx
@@ -11,23 +11,48 @@ import AddItem from '../pages/add-item/add-item';
 import NotFound from '../pages/not-found/not-found';
 import Order from '../pages/order/order';
 import OrderList from '../pages/order-list/order-list';
+import { AppRoute } from '../constants';
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Main />} />
-          <Route path="registration" element={<Registration />} />
-          <Route path="login" element={<Login />} />
-          <Route path="items" element={<ShopItemsList />} />
-          <Route path="item/:id" element={<ShopItem />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="add" element={<AddItem />} />
-          <Route path="edit/:id" element={<AddItem />} />
-          <Route path="orders" element={<OrderList />} />
-          <Route path="order" element={<Order />} />
-          <Route path="*" element={<NotFound />} />
+        <Route
+          path={AppRoute.Root}
+          element={<Layout/>}>
+          <Route
+            index
+            element={<Main/>}/>
+          <Route
+            path={AppRoute.Registration}
+            element={<Registration/>}/>
+          <Route
+            path={AppRoute.Login}
+            element={<Login/>}/>
+          <Route
+            path={AppRoute.ItemsList}
+            element={<ShopItemsList/>}/>
+          <Route
+            path={AppRoute.Item}
+            element={<ShopItem/>}/>
+          <Route
+            path={AppRoute.Cart}
+            element={<Cart/>}/>
+          <Route
+            path={AppRoute.AddItem}
+            element={<AddItem/>}/>
+          <Route
+            path={AppRoute.EditItem}
+            element={<AddItem/>}/>
+          <Route
+            path="orders"
+            element={<OrderList/>}/>
+          <Route
+            path={AppRoute.Order}
+            element={<Order/>}/>
+          <Route
+            path="*"
+            element={<NotFound/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ apps/webapp/src/constants.ts
@@ -0,0 +1,11 @@
+export enum AppRoute {
+  Root = '/',
+  Registration = 'registration',
+  Login = 'login',
+  ItemsList = 'items',
+  Item = 'item/:id',
+  Cart = 'cart',
+  AddItem = 'add',
+  EditItem = 'edit/:id',
+  Order = 'order',
+}
