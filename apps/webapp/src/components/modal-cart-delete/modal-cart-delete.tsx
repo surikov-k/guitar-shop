c17f162e20d7f0963f4492621f8e291d37f545d0
Konstantin Surikov <surikov@gmail.com>
2023-02-13 20:36:11 +0300

    ✨ [feat: scroll lock] add scroll lock hook

--- apps/webapp/src/components/modal-cart-delete/modal-cart-delete.tsx
+++ apps/webapp/src/components/modal-cart-delete/modal-cart-delete.tsx
@@ -9,13 +9,22 @@ type ModalTypeRemoveProps = {
 };
 
 export function ModalCartDelete({ shopItem }: ModalTypeRemoveProps) {
-  const { name } = shopItem;
   const { close } = useContext(ModalContext);
+
+  const {
+    name,
+    photo,
+    code,
+    type,
+    stringsNumber,
+    price,
+  } = shopItem;
 
   const deleteHandler = () => {
     console.log('delete');
     close();
   };
+
   return (
     <div className="modal is-active">
       <div className="modal__wrapper">
@@ -27,36 +36,38 @@ export function ModalCartDelete({ shopItem }: ModalTypeRemoveProps) {
           <div className="modal__info">
             <img
               className="modal__img"
-              src="img/content/catalog-product-1.png"
-              srcSet="img/content/catalog-product-1@2x.png 2x"
+              src={`assets/img/content/${photo}`}
+              srcSet={`img/content/${photo}@2x.png 2x`}
               width="67"
               height="137"
               alt="Честер bass"
             />
             <div className="modal__info-wrapper">
               <h3 className="modal__product-name title title--little title--uppercase">
-                Гитара Честер bass
+                Гитара {name}
               </h3>
               <p className="modal__product-params modal__product-params--margin-11">
-                Артикул: SO757575
+                Артикул: {code}
               </p>
-              <p className="modal__product-params">Электрогитара, 6 струнная</p>
+              <p className="modal__product-params">{type}, {stringsNumber} струнная</p>
               <p className="modal__price-wrapper">
                 <span className="modal__price">Цена:</span>
-                <span className="modal__price">17 500 ₽</span>
+                <span className="modal__price">{price} ₽</span>
               </p>
             </div>
           </div>
           <div className="modal__button-container">
             <button
               onClick={deleteHandler}
-              className="button button--small modal__button">
+              className="button button--small modal__button"
+            >
               Удалить товар
             </button>
-            <Link 
+            <Link
               to={AppRoute.Root}
               onClick={close}
-              className="button button--black-border button--small modal__button modal__button--right">
+              className="button button--black-border button--small modal__button modal__button--right"
+            >
               Продолжить покупки
             </Link>
           </div>
--- apps/webapp/src/contexts/modal-context/modal-context-provider.tsx
+++ apps/webapp/src/contexts/modal-context/modal-context-provider.tsx
@@ -1,5 +1,6 @@
 import { ModalContext } from "./modal-context";
 import { useState } from 'react';
+import { useScrollLock } from '../../hooks';
 
 type ModalProviderProps = {
   children: JSX.Element[]
@@ -9,20 +10,27 @@ export function ModalProvider({ children }: ModalProviderProps) {
   const [opened, setOpened] = useState<boolean>(false);
   const [content, setContent] = useState<JSX.Element | null>(null)
 
+  const lockScroll = useScrollLock();
+
   const open = (content: JSX.Element) => {
     setOpened(true);
     setContent(content);
+    lockScroll(true)
   }
 
-  const close = () => setOpened(false);
+  const close = () => {
+    setOpened(false);
+    lockScroll(false);
+  };
 
   return (
-      <ModalContext.Provider value={{ open: open, close }}>
-      {children}
+    <ModalContext.Provider value={{ open, close }}>
+      <>
+        {children}
         {
-          opened && <>{content}</>
-
+          opened && content
         }
-      </ModalContext.Provider>
+      </>
+    </ModalContext.Provider>
   );
 }
